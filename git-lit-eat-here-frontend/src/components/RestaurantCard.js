407815ca555d97b7dc022043a4977fbafc4101d3
Giovanni <nibius9@gmail.com>
2021-11-17 14:55:52 -0800

    Second

--- git-lit-eat-here-frontend/src/components/RestaurantCard.js
+++ git-lit-eat-here-frontend/src/components/RestaurantCard.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 function RestaurantCard( {rest: {name, review, cuisine}}) {
+    
+   function handleClick(e){
+        console.log(e)
+    }
+    
     return (
         <div>
             <div> Name: {name},  Cuisine: {cuisine}
+            <button onClick ={handleClick} className = "delete">x</button>
             </div>
-            
+        
             
         </div>
     )
